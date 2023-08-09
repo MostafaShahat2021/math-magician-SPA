d62245ab5afee93658f0ab29d19bb3d4accd9228
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-09 16:15:33 +0300

    Add Events & Logic

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,12 @@
+import PropTypes from 'prop-types';
 import './display.css';
 
-function Display() {
+function Display({ result }) {
   return (
-    <p className="display">0</p>
+    <p className="display">{result}</p>
   );
 }
-
+Display.propTypes = {
+  result: PropTypes.string.isRequired,
+};
 export default Display;
