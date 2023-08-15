f7aa8228b98ed11570257da05531e112a94fbbcb
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-15 12:12:53 +0300

    Update calculator component and add basic styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,25 @@
-import React from 'react';
-
 function Home() {
   return (
-    <>
-      <h2>Math Magicians</h2>
+    <div className="home-cont">
+      <div className="cont">
+        <h5>Welcome to our page</h5>
+      </div>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum
-        fuga animi quas! Ips illo iusto voluptate error pariatur cumque earum
-        neque quo suscipit, sit dicta accu harum ullam voluptates ex numquam
-        impedit aliquam rerum. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Officiis eum fuga animi quas! Ips illo iusto voluptate
-        error pariatur cumque earum neque quo suscipit,
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit, sit dicta accu
+        harum ullam voluptates ex numquam impedit aliquam rerum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit,
       </p>
+      <br />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum
-        fuga animi quas! Ips illo iusto voluptate error pariatur cumque earum
-        neque quo suscipit, sit dicta accu harum ullam voluptates ex numquam
-        impedit aliquam rerum. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Officiis eum fuga animi quas! Ips illo iusto voluptate
-        error pariatur cumque earum neque quo suscipit,
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit, sit dicta accu
+        harum ullam voluptates ex numquam impedit aliquam rerum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum fuga animi quas! Ips
+        illo iusto voluptate error pariatur cumque earum neque quo suscipit,
       </p>
-    </>
+    </div>
   );
 }
-
 export default Home;
