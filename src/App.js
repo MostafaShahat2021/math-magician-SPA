50ab8057d2ed2bdb494a9948dc8c9712a15452a9
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-15 12:25:10 +0300

    Add navbar with basic styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import CalcContainer from './components/Calculator';
 import Quote from './components/quote';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quote" element={<Quote />} />
