0ce10aca1973fede63b2c7005c87c909b34a4f03
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-15 11:39:45 +0300

    Add Home page Component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h2>Math Magicians</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum
+        fuga animi quas! Ips illo iusto voluptate error pariatur cumque earum
+        neque quo suscipit, sit dicta accu harum ullam voluptates ex numquam
+        impedit aliquam rerum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Officiis eum fuga animi quas! Ips illo iusto voluptate
+        error pariatur cumque earum neque quo suscipit,
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eum
+        fuga animi quas! Ips illo iusto voluptate error pariatur cumque earum
+        neque quo suscipit, sit dicta accu harum ullam voluptates ex numquam
+        impedit aliquam rerum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Officiis eum fuga animi quas! Ips illo iusto voluptate
+        error pariatur cumque earum neque quo suscipit,
+      </p>
+    </>
+  );
+}
+
+export default Home;
