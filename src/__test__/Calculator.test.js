e39e50a93dcdc4d65484153b9e7c6fb9c2452c78
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-16 18:48:35 +0330

    Change the message of testing

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
-it('..', () => {
+it('Calculator component rending test', () => {
   const CalcContainer = render(<Calculator />);
   expect(CalcContainer).toMatchSnapshot();
 });
